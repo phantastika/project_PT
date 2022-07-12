af207696898f54e2c4ad7a6788944b9ce38f17e1
Ol'ga <84837449+phantastika@users.noreply.github.com>
2022-07-12 13:47:16 +0300

    form updates

--- sass/script.js
+++ sass/script.js
@@ -1,36 +1,50 @@
 "use strict"
 
 document.addEventListener('DOMcontentloaded', function() {
-        const form = document.getElementById(contactform)
-        form.addEventListener('submit', formSend)
+            const form = document.getElementById(contactform)
+            form.addEventListener('submit', formSend)
 
-        async function formSend(e) {
-            e.preventDefault()
-            let error = formValidate(form)
-        }
+            async function formSend(e) {
+                e.preventDefault()
 
-        function formValidate(form) {
-            let error = 0
-            let formReq = document.querySelectorAll('_req')
-            for (let index = 0; index < formReq.length; index++)
-                const input = formReq[index]
-            formRemoveErrow(input)
-            if (input.classList.contains('_email'))
+                let error = formValidate(form)
+            }
+
+            function formValidate(form) {
+                let error = 0
+                let formReq = document.querySelectorAll('_req')
+                for (let index = 0; index < formReq.length; index++) {
+                    const input = formReq[index]
+                    formRemoveErrow(input)
+                    if (input.classList.contains('_email')) {
+                        if (emailTest(input)) {
+                            formAddErrow(input)
+                            error++
+                        }
+                    } else {
+                        if (input.value === '') {
+                            formAddErrow(input)
+                            error++
+                        }
+                    }
+
+                }
+                return error
 
 
+                function formAddErrow(input) {
+                    input.parentElement.classList.add('_error')
+                    input.classList.add('_error')
+                }
 
+                function formRemoveErrow(input) {
+                    input.parentElement.classList.remove('_error')
+                    input.classList.remove('_error')
+                }
 
-        }
+                function emailTest(input) {
+                    return !/^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+                }
+            }
 
-        function formAddErrow(input) {
-            input.parentElement.classList.add('_error')
-            input.classList.add('_error')
-        }
-
-        function formRemoveErrow(input) {
-            input.parentElement.classList.remove('_error')
-            input.classList.remove('_error')
-        }
-    }
-
-)
+        )
