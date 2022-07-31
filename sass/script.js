fe9f35ec0f362cebda08d3e0f0af820be631a6f2
Ol'ga <84837449+phantastika@users.noreply.github.com>
2022-07-31 18:47:17 +0300

    toggle menu

--- sass/script.js
+++ sass/script.js
@@ -1,50 +1,62 @@
 "use strict"
 
-document.addEventListener('DOMcontentloaded', function() {
-            const form = document.getElementById(contactform)
-            form.addEventListener('submit', formSend)
+// document.addEventListener('DOMcontentloaded', function() {
+//             const form = document.getElementById(contactform)
+//             form.addEventListener('submit', formSend)
 
-            async function formSend(e) {
-                e.preventDefault()
+//             async function formSend(e) {
+//                 e.preventDefault()
 
-                let error = formValidate(form)
-            }
+//                 let error = formValidate(form)
+//             }
 
-            function formValidate(form) {
-                let error = 0
-                let formReq = document.querySelectorAll('_req')
-                for (let index = 0; index < formReq.length; index++) {
-                    const input = formReq[index]
-                    formRemoveErrow(input)
-                    if (input.classList.contains('_email')) {
-                        if (emailTest(input)) {
-                            formAddErrow(input)
-                            error++
-                        }
-                    } else {
-                        if (input.value === '') {
-                            formAddErrow(input)
-                            error++
-                        }
-                    }
+//             function formValidate(form) {
+//                 let error = 0
+//                 let formReq = document.querySelectorAll('_req')
+//                 for (let index = 0; index < formReq.length; index++) {
+//                     const input = formReq[index]
+//                     formRemoveErrow(input)
+//                     if (input.classList.contains('_email')) {
+//                         if (emailTest(input)) {
+//                             formAddErrow(input)
+//                             error++
+//                         }
+//                     } else {
+//                         if (input.value === '') {
+//                             formAddErrow(input)
+//                             error++
+//                         }
+//                     }
 
-                }
-                return error
+//                 }
+//                 return error
 
 
-                function formAddErrow(input) {
-                    input.parentElement.classList.add('_error')
-                    input.classList.add('_error')
-                }
+//                 function formAddErrow(input) {
+//                     input.parentElement.classList.add('_error')
+//                     input.classList.add('_error')
+//                 }
 
-                function formRemoveErrow(input) {
-                    input.parentElement.classList.remove('_error')
-                    input.classList.remove('_error')
-                }
+//                 function formRemoveErrow(input) {
+//                     input.parentElement.classList.remove('_error')
+//                     input.classList.remove('_error')
+//                 }
 
-                function emailTest(input) {
-                    return !/^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
-                }
-            }
+//                 function emailTest(input) {
+//                     return !/^w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+//                 }
+//             }
 
-        )
+// //         )
+document.getElementById("navigation").addEventListener("click", ($event) => {
+    document.getElementById("navigation").querySelectorAll("a").forEach(element => {
+        element.classList.remove("active")
+        if (element.text === $event.target.firstChild.nodeValue) {
+            element.classList.add("active")
+        }
+
+    });
+
+})
+
+// console.log(document.getElementById("navigation"))
